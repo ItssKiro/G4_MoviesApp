848d9022bab73cee7f06597125a19526e49e150a
ItssKiro <113295300+ItssKiro@users.noreply.github.com>
2022-09-11 15:25:37 +0100

    Created App
    Created The Application Using Swift Ui

---
+++ Group4_MoviesApp/Group4_MoviesApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Group4_MoviesApp
+//
+//  Created by user206032 on 9/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Group4_MoviesApp/Group4_MoviesApp/Group4_MoviesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Group4_MoviesAppApp.swift
+//  Group4_MoviesApp
+//
+//  Created by user206032 on 9/11/22.
+//
+
+import SwiftUI
+
+@main
+struct Group4_MoviesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
